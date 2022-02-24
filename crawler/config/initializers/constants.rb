37753ff28f35fa88517d879ea5b6776c7ec050d2
Masahiro Aikawa <aikawame@wor.jp>
2022-02-24 22:22:34 +0900

    :zap: Railsの秘密情報を短い定数として定義する

---
+++ crawler/config/initializers/constants.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+CREDENTIALS = Rails.application.credentials
--- crawler/config/initializers/sentry.rb
+++ crawler/config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = Rails.application.credentials.sentry.dsn if Rails.env.production?
+  config.dsn = CREDENTIALS.sentry.dsn if Rails.env.production?
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.traces_sample_rate = 0
 end
