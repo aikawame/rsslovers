9c033125fe0c68731c56f980bd9e593478712801
Masahiro Aikawa <aikawame@wor.jp>
2022-02-26 09:40:00 +0900

    :sparkles: フィードへのお知らせ挿入機能を追加する

--- crawler/app/models/item.rb
+++ crawler/app/models/item.rb
@@ -6,6 +6,28 @@ require 'open-uri'
 class Item
   attr_reader :title, :description, :link_url, :updated_at
 
+  def self.all_notification_items
+    item_hashes = Utils.read_json_db(URI.parse("#{CREDENTIALS.s3.root_url}/rss/db/notifications.json"))
+    item_hashes.select { |item_hash| active?(item_hash) }
+               .map { |item_hash| create_notification_item(item_hash) }
+  end
+
+  def self.active?(item_hash)
+    now = Time.zone.now
+    start_at = Time.zone.parse(item_hash[:start_at])
+    end_at = Time.zone.parse(item_hash[:end_at])
+    hour_diff = ((now - start_at) / 1.hour.to_i).floor
+    start_at < now && now < end_at && (hour_diff % item_hash[:interval].to_i)
+  end
+
+  def self.create_notification_item(item_hash)
+    now = Time.zone.now
+    Item.new(title: item_hash[:title],
+             description: item_hash[:description],
+             link_url: URI.parse("#{item_hash[:link_url]}?ref=crawler&dt=#{now.strftime('%y%m%dH')}"),
+             updated_at: now)
+  end
+
   def initialize(title:, link_url:, updated_at:, description: nil)
     @title = title
     @description = description
--- crawler/lib/tasks/crawl.rake
+++ crawler/lib/tasks/crawl.rake
@@ -3,11 +3,13 @@
 desc 'Crawl specified sites and feeds immediately'
 task :crawl, %w[site feed] => :environment do |_, args|
   Rails.logger.info('クロールを開始します...')
+  notification_items = Item.all_notification_items
   Feed.where(args).shuffle.each do |feed|
     Rails.logger.info("#{feed.site.name} - #{feed.name}")
     feed.crawl_and_generate_items
     next unless feed.crawled
 
+    feed.items += notification_items if notification_items.present?
     feed.save
     Utils.purge_cdn_cache(feed.self_url)
   end
