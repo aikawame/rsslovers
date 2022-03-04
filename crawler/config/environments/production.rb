ccfb6b513faea243fec91e6bfd2bff9f5674c289
Masahiro Aikawa <aikawame@wor.jp>
2022-03-04 23:22:08 +0900

    :adhesive_bandage: 本番環境のログレベルを修正する

--- crawler/config/environments/production.rb
+++ crawler/config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :error
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
