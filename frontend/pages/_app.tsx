a8a47a88989314771b396e9146e76ae8eba51d80
Masahiro Aikawa <aikawame@wor.jp>
2022-03-27 23:07:46 +0900

    :sparkles: 共通レイアウトを追加する

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import Layout from '../components/Layout'
+import '../styles/globals.scss'
+import '../styles/icons.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
-
-export default MyApp
