c63d347111131b092dd5523841f5eebe53dc4272
Masahiro Aikawa <aikawame@wor.jp>
2022-07-18 13:31:27 +0900

    :adhesive_bandage: Sentryのデバッグログを本番以外で出さないようにする

--- crawler/config/initializers/sentry.rb
+++ crawler/config/initializers/sentry.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.dsn = CREDENTIALS.sentry.dsn if Rails.env.production?
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.traces_sample_rate = 0
+if Rails.env.production?
+  Sentry.init do |config|
+    config.dsn = CREDENTIALS.sentry.dsn
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.traces_sample_rate = 0
+  end
 end
