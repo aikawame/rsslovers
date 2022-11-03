ba1d257e767e86c44f934ea57a1d1fc8eb929ede
Masahiro Aikawa <aikawame@wor.jp>
2022-11-03 11:11:53 +0900

    :adhesive_bandage: 47NEWSの構造変更に対応する

--- crawler/app/models/crawler/yonnana.rb
+++ crawler/app/models/crawler/yonnana.rb
@@ -3,9 +3,9 @@
 # 47NEWSのクローラー
 class Crawler::Yonnana < Crawler
   def fetch_items
-    fetch_html.css('.title-list li').map do |block|
-      Item.new(title: block.css('h3.txt40').text,
-               link_url: URI.join(root_url, block.css('a').attr('href')),
+    fetch_html.css('.post_items .post_item').map do |block|
+      Item.new(title: block.css('.item_ttl').text,
+               link_url: URI.join(root_url, block.attr('href')),
                updated_at: Time.zone.now.midnight)
     end
   end
