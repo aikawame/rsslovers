2977f5921720e9c2cd014dada4526917133ddf63
Masahiro Aikawa <aikawame@wor.jp>
2022-12-21 21:00:19 +0900

    :adhesive_bandage: 産経ニュースの構造変更に対応する

--- crawler/app/models/crawler/sankei.rb
+++ crawler/app/models/crawler/sankei.rb
@@ -6,10 +6,10 @@ class Crawler::Sankei < Crawler
 
   def fetch_items
     selected_blocks = fetch_html.css('.grid__column-left-content article.storycard').select do |block|
-      block.css('h3 a').text.present?
+      block.css('h2 a, h3 a').text.present?
     end
     selected_blocks.map do |block|
-      anchor = block.css('h3 a')
+      anchor = block.css('h2 a, h3 a')
       datetime = block.css('time').attr('datetime')
       Item.new(title: anchor.text,
                link_url: URI.join(root_url, anchor.attr('href')),
