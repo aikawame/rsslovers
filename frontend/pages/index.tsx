8ac2dcbafb6b2427f54d8017ccef6d294481d315
Masahiro Aikawa <aikawame@wor.jp>
2023-10-01 21:57:26 +0900

    :adhesive_bandage: ロイターに関するお知らせを削除する

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -38,12 +38,6 @@ export default function Home() {
 
   return (
     <Main title="">
-      <Section heading="ロイターのフィードに配信関しまして">
-        <p>
-          現在、ロイターのサイト刷新によりフィードが正常に配信できない状況となっております。
-          本サービスでは、ロイターのフィードが正常に配信できるよう、対応を検討しております。
-        </p>
-      </Section>
       { data.map(category => { return (
         <Section heading={category.name} key={category.label}>
           <div className="panels">
