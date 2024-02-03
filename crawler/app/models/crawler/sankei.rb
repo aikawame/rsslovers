d5562b5be0d614b83aac1035b1b4482d294c0798
Masahiro Aikawa <aikawame@wor.jp>
2024-02-03 13:07:15 +0900

    :sparkles: 産経新聞のサイト刷新に対応する

--- crawler/app/models/crawler/sankei.rb
+++ crawler/app/models/crawler/sankei.rb
@@ -5,15 +5,10 @@ class Crawler::Sankei < Crawler
   prepend DynamicCrawler
 
   def fetch_items
-    selected_blocks = fetch_html.css('.grid__column-left-content article.storycard').select do |block|
-      block.css('h2 a, h3 a').text.present?
-    end
-    selected_blocks.map do |block|
-      anchor = block.css('h2 a, h3 a')
-      datetime = block.css('time').attr('datetime')
-      Item.new(title: anchor.text,
-               link_url: URI.join(root_url, anchor.attr('href')),
-               updated_at: Time.zone.parse(datetime))
+    fetch_html.css('article').map do |block|
+      Item.new(title: block.css('h2 a').text,
+               link_url: URI.join('https://www.sankei.com', block.css('h2 a').attr('href')),
+               updated_at: Time.zone.parse(block.css('time').attr('datetime')))
     end
   end
 end
