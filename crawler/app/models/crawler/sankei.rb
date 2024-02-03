1f3fe0880d0eed26900a9621f10669759da28c25
Masahiro Aikawa <aikawame@wor.jp>
2024-02-03 14:57:14 +0900

    :adhesive_bandage: 産経新聞のクローラーを修正する

--- crawler/app/models/crawler/sankei.rb
+++ crawler/app/models/crawler/sankei.rb
@@ -5,7 +5,10 @@ class Crawler::Sankei < Crawler
   prepend DynamicCrawler
 
   def fetch_items
-    fetch_html.css('article').map do |block|
+    selected_blocks = fetch_html.css('article').select do |block|
+      block.css('h2 a').attr('href').present?
+    end
+    selected_blocks.map do |block|
       Item.new(title: block.css('h2 a').text,
                link_url: URI.join('https://www.sankei.com', block.css('h2 a').attr('href')),
                updated_at: Time.zone.parse(block.css('time').attr('datetime')))
