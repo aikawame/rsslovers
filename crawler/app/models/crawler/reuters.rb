a368b68c4885ec443a2d7ddcce80dde5c49b2564
Masahiro Aikawa <aikawame@wor.jp>
2025-06-14 13:05:32 +0900

    :adhesive_bandage: ロイターのクローラーからログを外す

--- crawler/app/models/crawler/reuters.rb
+++ crawler/app/models/crawler/reuters.rb
@@ -6,10 +6,8 @@ class Crawler::Reuters < Crawler
 
   def fetch_items
     selector_str = "div[data-testid='HeroCard'], div[data-testid='BasicCard'], div[data-testid='HubCard']"
-    Rails.logger.info('  ' + fetch_html.css(selector_str).to_s)
     fetch_html.css(selector_str).map do |block|
       title = block.css("*[data-testid='Title']")
-      Rails.logger.info('  ' + title)
       anchor = title.attr('href')
       Item.new(title: title.text.strip,
                link_url: URI.join(root_url, anchor.text),
