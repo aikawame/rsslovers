4f361c3c2577f6e54f6c6e50201e18e47846b7f6
Masahiro Aikawa <aikawame@wor.jp>
2025-09-24 23:32:42 +0900

    Reapply "✨ ロイターのクローラーを復活する"
    This reverts commit c8d9eb00a2bd2f181e476205b6a19f8946dcee3a.

--- crawler/app/models/crawler/reuters.rb
+++ crawler/app/models/crawler/reuters.rb
@@ -5,12 +5,10 @@ class Crawler::Reuters < Crawler
   prepend DynamicCrawler
 
   def fetch_items
-    selector_str = "div[data-testid='HeroCard'], div[data-testid='BasicCard'], div[data-testid='HubCard']"
-    fetch_html.css(selector_str).map do |block|
-      title = block.css("*[data-testid='Title']")
-      anchor = title.attr('href')
+    fetch_html.css("div[data-testid='MediaStoryCard']").map do |block|
+      title = block.css("*[data-testid='Heading']")
       Item.new(title: title.text.strip,
-               link_url: URI.join(root_url, anchor.text),
+               link_url: URI.join(root_url, title.attr('href')&.text || title.css('a').attr('href').text),
                updated_at: Time.zone.parse(block.css('time').attr('datetime')))
     end
   end
