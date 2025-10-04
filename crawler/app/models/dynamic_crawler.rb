5355f3236dba75eb0dac4cfa4e5870aed20256c0
Masahiro Aikawa <aikawame@wor.jp>
2025-10-05 06:25:17 +0900

    :adhesive_bandage: クローラーの設定を修正する

--- crawler/app/models/dynamic_crawler.rb
+++ crawler/app/models/dynamic_crawler.rb
@@ -3,6 +3,7 @@
 # 動的HTMLを扱うクローラー
 module DynamicCrawler
   def fetch_html
+    FileUtils.rm_rf('/tmp/*')
     session = Capybara::Session.new(:crawler)
     session.visit(@url)
     result = Nokogiri::HTML.parse(session.html)
