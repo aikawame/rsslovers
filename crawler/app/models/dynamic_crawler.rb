f6bd89fe2546de37aa3e42c4bf26e1791d5a0aea
Masahiro Aikawa <aikawame@wor.jp>
2025-10-11 17:56:10 +0900

    :sparkles: ブラウザーをFirefoxに変更する

--- crawler/app/models/dynamic_crawler.rb
+++ crawler/app/models/dynamic_crawler.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 
+require 'selenium-webdriver'
+
 # 動的HTMLを扱うクローラー
 module DynamicCrawler
   def fetch_html
-    session = Capybara::Session.new(:scraper)
-    session.visit(@url)
-    result = Nokogiri::HTML.parse(session.html)
-    session.quit
+    driver = Selenium::WebDriver.for(:firefox, options:)
+    driver.navigate.to(@url)
+    result = Nokogiri::HTML.parse(driver.page_source)
+    driver.quit
     result
+  end
+
+  private
+
+  def options
+    Selenium::WebDriver::Firefox::Options.new.tap do |options|
+      options.add_argument('-headless')
+    end
   end
 end
