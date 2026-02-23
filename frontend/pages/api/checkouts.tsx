f3e58b73ba73b3676ec7b2a88a6c6273d3832992
Masahiro Aikawa <aikawame@wor.jp>
2026-02-23 12:16:22 +0900

    :arrow_up: Stripeの最新バージョンに対応する

--- frontend/pages/api/checkouts.tsx
+++ frontend/pages/api/checkouts.tsx
@@ -7,7 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     const { price } = req.query
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2025-12-15.clover'
+      apiVersion: '2026-01-28.clover'
     })
     const session = await stripe.checkout.sessions.create({
       line_items: [
