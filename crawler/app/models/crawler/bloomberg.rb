ccd00c2b180381f3aa8dd0f4a240f0f337908ee1
Masahiro Aikawa <aikawame@wor.jp>
2026-07-18 14:33:41 +0900

    :adhesive_bandage: ブルームバーグのクローラーを修正する

--- crawler/app/models/crawler/bloomberg.rb
+++ crawler/app/models/crawler/bloomberg.rb
@@ -5,7 +5,7 @@ class Crawler::Bloomberg < Crawler
   prepend DynamicCrawler
 
   def fetch_items
-    fetch_html.css('.styles_itemTextContainer__bXUtK').map do |block|
+    fetch_html.css('.styles_itemContainer__t2ZQc').map do |block|
       anchor = block.css('a')
       Item.new(title: anchor.text,
                link_url: URI.join(root_url, anchor.attr('href')),
